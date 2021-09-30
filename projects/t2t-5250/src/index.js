f0ccb2a8c6dd7e2abe4fd748403997a32b4ce25d
AhmedAGadir <ahmed.gadir@gmail.com>
2021-09-30 12:24:38 +0100

    update

--- projects/t2t-5250/src/index.js
+++ projects/t2t-5250/src/index.js
@@ -1,79 +1,84 @@
 'use strict';
 
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { AgGridReact, AgGridColumn } from 'ag-grid-react';
 import 'ag-grid-enterprise';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import numeral from 'numeral';
+class GridExample extends Component {
+  constructor(props) {
+    super(props);
 
-const GridExample = () => {
-  const [gridApi, setGridApi] = useState(null);
-  const [gridColumnApi, setGridColumnApi] = useState(null);
-  const [rowData, setRowData] = useState(null);
+    this.state = {
+      defaultColDef: {
+        flex: 1,
+        width: 90,
+        resizable: true
+      },
+      rowData: null,
+      counter: 0
+    };
+  }
 
-  const onGridReady = (params) => {
-    setGridApi(params.api);
-    setGridColumnApi(params.columnApi);
+  numberValueFormatter = (params) => {
+    return params.value ? numeral(params.value).multiply(100).format('0,0.000') : '--';
+  }
+
+  customSumFunction = (params) => {
+    var sum = 0;
+    //console.log(params);
+    params.values.forEach(function (value) { sum += Number(value); });
+    return sum;
+  }
+
+  onGridReady = (params) => {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
 
     const updateData = (data) => {
-      setRowData(data);
+      this.setState({ rowData: data });
     };
 
     fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
       .then((resp) => resp.json())
-      .then((data) => {
-        data = data.map(row => ({
-          ...row,
-          athlete: (row.athlete + ' ').repeat(5)
-        }));
-        updateData(data)
-      });
+      .then((data) => updateData(data));
   };
 
-  const updateData = () => {
-    let updatedData = rowData.map(row => ({
-      ...row,
-      age: row.age + 1
-    }));
-    setRowData(updatedData);
-  }
-
-  return (
-    <div style={{ width: '100%', height: '100%' }}>
-      <button onClick={updateData}>update data</button>
-      <div
-        id="myGrid"
-        style={{
-          height: '100%',
-          width: '100%',
-        }}
-        className="ag-theme-alpine"
-      >
-        <AgGridReact
-          defaultColDef={{
-            flex: 1,
-            minWidth: 100,
-            resizable: true
+  render() {
+    return (
+      <div style={{ width: '100%', height: '100%' }}>
+        <button onClick={() => this.setState(state => ({ counter: state.counter + 1 }))}>Increment Counter</button>
+        <p>counter: {this.state.counter}</p>
+        <div
+          id="myGrid"
+          style={{
+            height: '100%',
+            width: '100%',
           }}
-          enableRangeSelection={true}
-          onGridReady={onGridReady}
-          rowData={rowData}
+          className="ag-theme-alpine"
         >
-          <AgGridColumn field="athlete" minWidth={150} />
-          <AgGridColumn field="age" maxWidth={90} />
-          <AgGridColumn field="country" minWidth={150} />
-          <AgGridColumn field="year" maxWidth={90} />
-          <AgGridColumn field="date" minWidth={150} />
-          <AgGridColumn field="sport" minWidth={150} />
-          <AgGridColumn field="gold" />
-          <AgGridColumn field="silver" />
-          <AgGridColumn field="bronze" />
-          <AgGridColumn field="total" />
-        </AgGridReact>
+          <AgGridReact
+            defaultColDef={this.state.defaultColDef}
+            enableRangeSelection={true}
+            onGridReady={this.onGridReady}
+            rowData={this.state.rowData}
+          >
+            <AgGridColumn field='athlete' />
+            <AgGridColumn field='country' rowGroup={true} />
+            <AgGridColumn field='age' valueFormatter={this.numberValueFormatter} aggFunc={this.customSumFunction} />
+            <AgGridColumn field='year' valueFormatter={this.numberValueFormatter} aggFunc={this.customSumFunction} />
+            <AgGridColumn field='gold' valueFormatter={this.numberValueFormatter} aggFunc={this.customSumFunction} />
+            <AgGridColumn field='silver' valueFormatter={this.numberValueFormatter} aggFunc={this.customSumFunction} />
+            <AgGridColumn field='bronze' valueFormatter={this.numberValueFormatter} aggFunc={this.customSumFunction} />
+            <AgGridColumn field='total' valueFormatter={this.numberValueFormatter} aggFunc={this.customSumFunction} />
+
+          </AgGridReact>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 render(<GridExample></GridExample>, document.querySelector('#root'));
