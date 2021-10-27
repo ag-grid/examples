2021e2fc73733cd976ebb622f0434b48493f9a4b
AhmedAGadir <ahmed.gadir@gmail.com>
2021-10-27 11:15:00 +0100

    update

--- projects/t2t-5472/src/app/app.module.ts
+++ projects/t2t-5472/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
 
 import 'ag-grid-enterprise'
 import { LicenseManager } from 'ag-grid-enterprise';
-LicenseManager.setLicenseKey('CompanyName=Softchoice LP (Toronto)_on_behalf_of_Mother Parkers Tea & Coffee Inc,LicensedApplication=Alletec,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=1,LicensedProductionInstancesCount=0,AssetReference=AG-016255,ExpiryDate=9_June_2022_[v2]_MTY1NDcyOTIwMDAwMA==4eb0bc07d3b0c703072add2e87e682e5');
+LicenseManager.setLicenseKey('');
 
 
 @NgModule({
