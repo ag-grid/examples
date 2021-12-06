0fb1014809c52abb98306a168a709da55259f605
AhmedAGadir <ahmed.gadir@gmail.com>
2021-12-06 14:17:43 +0000

    update

--- projects/test-angular/src/app/app.component.ts
+++ projects/test-angular/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from "@angular/core";
-import "ag-grid-enterprise";
-import { HttpClient } from "@angular/common/http";
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'ag-grid-enterprise';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 @Component({
-  selector: "my-app",
+  selector: 'my-app',
   template: `<ag-grid-angular
     #agGrid
-    style="width: 90%; height: 500px;"
+    style="width: 100%; height: 100%;"
     id="myGrid"
     class="ag-theme-alpine"
     [columnDefs]="columnDefs"
@@ -16,58 +16,59 @@ import "ag-grid-community/dist/styles/ag-theme-alpine.css";
     [enableRangeSelection]="true"
     [rowData]="rowData"
     (gridReady)="onGridReady($event)"
-  ></ag-grid-angular>`
+  ></ag-grid-angular>`,
 })
 export class AppComponent {
-  public gridApi: any;
-  public gridColumnApi: any;
-  public columnDefs: any;
-  public defaultColDef: any;
+  public gridApi;
+  public gridColumnApi;
+
+  public columnDefs;
+  public defaultColDef;
   public rowData: any;
 
-  constructor(private http: HttpClient) {
+  constructor(public http: HttpClient) {
     this.columnDefs = [
       {
-        field: "athlete",
-        minWidth: 150
+        field: 'athlete',
+        minWidth: 150,
       },
       {
-        field: "age",
-        maxWidth: 90
+        field: 'age',
+        maxWidth: 90,
       },
       {
-        field: "country",
-        minWidth: 150
+        field: 'country',
+        minWidth: 150,
       },
       {
-        field: "year",
-        maxWidth: 90
+        field: 'year',
+        maxWidth: 90,
       },
       {
-        field: "date",
-        minWidth: 150
+        field: 'date',
+        minWidth: 150,
       },
       {
-        field: "sport",
-        minWidth: 150
+        field: 'sport',
+        minWidth: 150,
       },
-      { field: "gold" },
-      { field: "silver" },
-      { field: "bronze" },
-      { field: "total" }
+      { field: 'gold' },
+      { field: 'silver' },
+      { field: 'bronze' },
+      { field: 'total' },
     ];
     this.defaultColDef = {
       flex: 1,
-      minWidth: 100
+      minWidth: 100,
     };
   }
 
-  onGridReady(params: { api: any; columnApi: any }) {
+  onGridReady(params) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
 
-    this.http.get("https://www.ag-grid.com/example-assets/olympic-winners.json").subscribe((data) => {
-      this.rowData = data;
-    });
+    this.http
+      .get('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .subscribe((data) => params.api.setRowData(data));
   }
 }
