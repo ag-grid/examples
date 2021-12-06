b25914dc4706a9503a5c6cba3f5c702ff475f307
AhmedAGadir <ahmed.gadir@gmail.com>
2021-12-06 10:58:44 +0000

    commit

--- scripts/helpers/play.js
+++ scripts/helpers/play.js
@@ -1,32 +1,4 @@
-// console.log(123, __dirname);
-// console.log(456, process.cwd());
-
-
-// ***********************************
-// testing promises
-
-// let promiseArr = [];
-
-// for (let i = 0; i < 5; i++) {
-//     let promise = new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve(i);
-//         }, i * 1000);
-//     })
-//     promiseArr.push(promise)
-// }
-
-// Promise.all(promiseArr)
-//     .then(result => {
-//         console.log('result is', result);
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     });
-
-
-// ***********************************
-// step 1) read files 
+// // step 1) read files 
 
 // const fs = require("fs");
 
@@ -85,48 +57,307 @@
 // let data = getPostData();
 
 
+
+// =================================================================
+// =================================================================
+
+
+var fs = require('fs');
+
+var htmlContent = `
+<html>
+    <head></head>
+    <body>
+        <script>
+            function getExampleFiles() {
+                let exampleFiles = {
+                    'index.html': {
+                        source: 'hello world',
+                        // source: indexSource,
+                        isFramework: false,
+                    },
+                    'main.js': {
+                        source: 'goodbye world',
+                        // source: mainSource,
+                        isFramework: false,
+                    },
+                };
+                return new Promise((resolve, reject) => {
+                    resolve(exampleFiles);
+                });
+            }
+
+
+            function submitForm() {
+                const title = 'Range selection test';
+
+                getExampleFiles().then((files) => {
+                    console.log('exampleFiles', files);
+                    const form = document.createElement('form');
+                    form.method = 'post';
+                    form.style.display = 'none';
+                    form.action = 'https://plnkr.co/edit/?p=preview';
+                    // form.target = '_blank';
+
+                    const addHiddenInput = (name, value) => {
+                        const input = document.createElement('input');
+                        input.type = 'hidden';
+                        input.name = name;
+                        input.value = value;
+
+                        form.appendChild(input);
+                    };
+
+                    addHiddenInput('tags[0]', 'ag-grid');
+                    addHiddenInput('tags[1]', 'example');
+                    addHiddenInput('private', true);
+                    addHiddenInput('description', title);
+
+                    Object.keys(files).forEach((key) => {
+                        addHiddenInput(\`files[\${key}]\`, files[key].source);
+                    });
+
+                    document.body.appendChild(form);
+                    console.log('submitting form', form);
+                    // debugger;
+                    form.submit();
+                    document.body.removeChild(form);
+                });
+            };
+
+            submitForm()
+
+        </script>
+    </body>
+</html>`
+
+fs.writeFile('my-page.html', htmlContent, (error) => { /* handle error */ });
+
+
+const http = require('http')
+// const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('my-page.html').pipe(res)
+})
+
+server.listen(process.env.PORT || 3000)
+
+const open = require('open');
+
+// open('https://localhost:3000')
+
+// ************************************************************************
+// ************************************************************************
+// open it with a server, and serve the files and then close the server straight away
+// ************************************************************************
+// ************************************************************************
+
+
+
+
+
+
+
+// let http = require('http');
+
+// let handleRequest = (request, response) => {
+//     response.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//     });
+//     response.write('Hi There!');
+//     response.end();
+// };
+
+// http.createServer(handleRequest).listen(8000);
+
+
+
+
+
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     res.write('<html><head></head><body>');
+//     res.write('<p>Write your HTML content here</p>');
+//     res.write(`<script>
+//     function getExampleFiles() {
+//         let exampleFiles = {
+//             'index.html': {
+//                 source: 'hello world',
+//                 // source: indexSource,
+//                 isFramework: false,
+//             },
+//             'main.js': {
+//                 source: 'goodbye world',
+//                 // source: mainSource,
+//                 isFramework: false,
+//             },
+//         };
+
+//         return new Promise((resolve, reject) => {
+//             resolve(exampleFiles);
+//         });
+//     }
+
+
+//     function submitForm() {
+//         const title = 'Range selection test';
+
+//         getExampleFiles().then((files) => {
+//             console.log('exampleFiles', files);
+//             const form = document.createElement('form');
+//             form.method = 'post';
+//             form.style.display = 'none';
+//             form.action = 'https://plnkr.co/edit/?p=preview';
+//             // form.target = '_blank';
+
+//             const addHiddenInput = (name, value) => {
+//                 const input = document.createElement('input');
+//                 input.type = 'hidden';
+//                 input.name = name;
+//                 input.value = value;
+
+//                 form.appendChild(input);
+//             };
+
+//             addHiddenInput('tags[0]', 'ag-grid');
+//             addHiddenInput('tags[1]', 'example');
+//             addHiddenInput('private', true);
+//             addHiddenInput('description', title);
+
+//             Object.keys(files).forEach((key) => {
+//                 addHiddenInput(\`files[\${key}]\`, files[key].source);
+//             });
+
+//             document.body.appendChild(form);
+//             console.log('submitting form', form);
+//             // debugger;
+//             form.submit();
+//             document.body.removeChild(form);
+//         });
+//     };
+
+//     submitForm()
+
+//     </script>`);
+//     res.end('</body></html>');
+// }).listen(1337);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(123, __dirname);
+// console.log(456, process.cwd());
+
+
+// ***********************************
+// testing promises
+
+// let promiseArr = [];
+
+// for (let i = 0; i < 5; i++) {
+//     let promise = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(i);
+//         }, i * 1000);
+//     })
+//     promiseArr.push(promise)
+// }
+
+// Promise.all(promiseArr)
+//     .then(result => {
+//         console.log('result is', result);
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     });
+
+
+// ***********************************
+
+
+
 // ***********************************
 // step 2) post request
 
-const https = require('https')
+// const https = require('https')
 
-let data = JSON.stringify({
-    "index.html": { isFramework: false, source: 'Hello World!' },
-    "tags[0]": "ag-grid",
-    "tags[1]": "example",
-    "private": true,
-    "description": "test",
-    "files[index.html]": 'Hello World!',
-    "files[main.js]": "console.log('Laow G!')"
-});
+// let data = JSON.stringify({
+//     "index.html": { isFramework: false, source: 'Hello World!' },
+//     "tags[0]": "ag-grid",
+//     "tags[1]": "example",
+//     "private": true,
+//     "description": "test",
+//     "files[index.html]": 'Hello World!',
+//     "files[main.js]": "console.log('Laow G!')"
+// });
 
-const options = {
-    // hostname: 'http://plnkr.co/edit/?p=preview',
-    hostname: 'plnkr.co',
-    //   port: 443,
-    path: 'edit/?p=preview',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': data.length
-    }
-}
+// const options = {
+//     // hostname: 'http://plnkr.co/edit/?p=preview',
+//     hostname: 'plnkr.co',
+//     //   port: 443,
+//     path: 'edit/?p=preview',
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Content-Length': data.length
+//     }
+// }
 
-const req = https.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`)
+// const req = https.request(options, res => {
+//     console.log(`statusCode: ${ res.statusCode }`)
 
-    res.on('data', d => {
-        process.stdout.write(d)
-    })
-})
+//     res.on('data', d => {
+//         process.stdout.write(d)
+//     })
+// })
 
-req.on('error', error => {
-    console.error(error)
-})
+// req.on('error', error => {
+//     console.error(error)
+// })
 
-// req.write(JSON.stringify(data))
-req.write(data);
-req.end()
+// // req.write(JSON.stringify(data))
+// req.write(data);
+// req.end()
 
 
 
@@ -180,7 +411,7 @@ req.end()
 //         addHiddenInput('description', title);
 
 //         Object.keys(files).forEach((key) => {
-//             addHiddenInput(`files[${key}]`, files[key].source);
+//             addHiddenInput(`files[${ key }]`, files[key].source);
 //         });
 
 //         document.body.appendChild(form);
