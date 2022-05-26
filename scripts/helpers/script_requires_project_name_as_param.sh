205cef55c65fcebfb096b5288e28701f48310d86
shuheb <37620997+shuheb@users.noreply.github.com>
2022-05-26 14:24:00 +0100

    clean up

--- scripts/helpers/script_requires_project_name_as_param.sh
+++ scripts/helpers/script_requires_project_name_as_param.sh
@@ -2,8 +2,8 @@
 
 # FRAMEWORK=$( echo $1 | gsed -n -e 's/--\(react\).*/\1/p' -e 's/--\(angular\).*/\1/p' -e 's/--\(vue\).*/\1/p' -e 's/--\(vanilla\).*/\1/p')
 
-if [ -z $2 ]; then
-    echo -e "Please specify the project directory:\n  ${CYAN}t2-create-$1 ${GREEN}<project-directory>"
-    echo -e "\n${NOCOLOR}For example:\n  ${CYAN}t2-create-$1 ${GREEN}my-$1-app"
+if [ -z $1 ]; then
+    echo -e "Please specify the project directory:\n  ${CYAN}t2-create-$FRAMEWORK ${GREEN}<project-directory>"
+    echo -e "\n${NOCOLOR}For example:\n  ${CYAN}t2-create-$FRAMEWORK ${GREEN}my-$FRAMEWORK-app"
     exit 1
 fi
