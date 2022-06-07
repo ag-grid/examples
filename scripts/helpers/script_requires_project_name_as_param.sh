a9125671786e2da1a28b4f3583cc645570b9a56e
shuheb <37620997+shuheb@users.noreply.github.com>
2022-06-07 13:57:24 +0100

    modules first iteration

--- scripts/helpers/script_requires_project_name_as_param.sh
+++ scripts/helpers/script_requires_project_name_as_param.sh
@@ -3,7 +3,7 @@
 # FRAMEWORK=$( echo $1 | gsed -n -e 's/--\(react\).*/\1/p' -e 's/--\(angular\).*/\1/p' -e 's/--\(vue\).*/\1/p' -e 's/--\(vanilla\).*/\1/p')
 
 if [ -z $1 ]; then
-    echo -e "Please specify the project directory:\n  ${CYAN}t2-create-$FRAMEWORK ${GREEN}<project-directory>"
-    echo -e "\n${NOCOLOR}For example:\n  ${CYAN}t2-create-$FRAMEWORK ${GREEN}my-$FRAMEWORK-app"
+    echo -e "Please specify the project directory:\n  ${CYAN}t2-create-$FRAMEWORK ${GREEN}<project-directory> <install-method>"
+    echo -e "\n${NOCOLOR}For example:\n  ${CYAN}t2-create-$FRAMEWORK ${GREEN}my-$FRAMEWORK-app\n  ${CYAN}t2-create-$FRAMEWORK ${GREEN}my-$FRAMEWORK-app --modules"
     exit 1
 fi
