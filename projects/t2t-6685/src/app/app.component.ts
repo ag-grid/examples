d369d8b1a07606cfadbbd413c4926b25a4a899d6
chefnoname <abdijama21@gmail.com>
2022-06-08 10:50:22 +0100

    update ticket

---
+++ projects/t2t-6685/src/app/app.component.ts
@@ -0,0 +1,78 @@
+import { Component } from "@angular/core";
+import "ag-grid-enterprise";
+import { HttpClient } from "@angular/common/http";
+
+LicenseManager.setLicenseKey(
+  'CompanyName=SoftwareONE Inc._on_behalf_of_Macquarie Global,LicensedGroup=Macquarie CGM Technology UX,LicenseType=MultipleApplications,LicensedConcurrentDeveloperCount=35,LicensedProductionInstancesCount=1,AssetReference=AG-017044,ExpiryDate=8_November_2022_[v2]_MTY2Nzg2NTYwMDAwMA==db746e75045c67b6e7d0c34472e5ff57'
+);
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import { LicenseManager } from "ag-grid-enterprise";
+@Component({
+  selector: "my-app",
+  template: `<ag-grid-angular
+    #agGrid
+    style="width: 90%; height: 500px;"
+    id="myGrid"
+    class="ag-theme-alpine"
+    [columnDefs]="columnDefs"
+    [defaultColDef]="defaultColDef"
+    [enableRangeSelection]="true"
+    [rowData]="rowData"
+    (gridReady)="onGridReady($event)"
+  ></ag-grid-angular>`
+})
+export class AppComponent {
+  public gridApi: any;
+  public gridColumnApi: any;
+  public columnDefs: any;
+  public defaultColDef: any;
+  public rowData: any;
+
+  constructor(private http: HttpClient) {
+    this.columnDefs = [
+      {
+        field: "athlete",
+        minWidth: 150
+      },
+      {
+        field: "age",
+        maxWidth: 90
+      },
+      {
+        field: "country",
+        minWidth: 150
+      },
+      {
+        field: "year",
+        maxWidth: 90
+      },
+      {
+        field: "date",
+        minWidth: 150
+      },
+      {
+        field: "sport",
+        minWidth: 150
+      },
+      { field: "gold" },
+      { field: "silver" },
+      { field: "bronze" },
+      { field: "total" }
+    ];
+    this.defaultColDef = {
+      flex: 1,
+      minWidth: 100
+    };
+  }
+
+  onGridReady(params: { api: any; columnApi: any }) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    this.http.get("https://www.ag-grid.com/example-assets/olympic-winners.json").subscribe((data) => {
+      this.rowData = data;
+    });
+  }
+}
