6c14a440093d7cbc03bfe97d5083fedf3d6f49a6
shuheb <37620997+shuheb@users.noreply.github.com>
2022-06-09 13:27:56 +0100

    cleanup

--- scripts/helpers/requiresPackagesOrModules.sh
+++ scripts/helpers/requiresPackagesOrModules.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 AG_GRID_METHOD=$(echo $2 | gsed -n -e 's/--\(packages\).*/\1/p' -e 's/--\(modules\).*/\1/p')
-echo $PWD
+
 if [[ ! -z "$2" && -z "$AG_GRID_METHOD" ]]; then
     echo -e "You must supply an AG Grid installation method"
     echo -e "For example: ${CYAN}--packages or --modules"
