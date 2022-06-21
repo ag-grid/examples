027bac70be5c051caecc04ef177d6c78790cdd87
abdiWali <abdicodes94@gmail.com>
2022-06-21 13:03:15 +0100

    updated ticket t2t-6750

---
+++ projects/t2t-6750/src/App.js
@@ -0,0 +1,80 @@
+"use strict";
+
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { render } from "react-dom";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-enterprise";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+const App = () => {
+  const gridRef = useRef();
+  const containerStyle = useMemo(
+    () => ({ width: "100%", height: "1000px" }),
+    []
+  );
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const [rowData, setRowData] = useState();
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "country", rowGroup: true, hide: true },
+    { field: "year", rowGroup: true, hide: true },
+    { field: "athlete" },
+    { field: "sport" },
+    { field: "total" },
+  ]);
+  const defaultColDef = useMemo(() => {
+    return {
+      flex: 1,
+      minWidth: 100,
+      sortable: true,
+      resizable: true,
+    };
+  }, []);
+
+  const autoGroupColumnDef = useMemo(() => {
+    return {
+      headerValueGetter: (params) => `${params.colDef.headerName} Group Column`,
+      minWidth: 220,
+      cellRendererParams: {
+        suppressCount: true,
+        checkbox: true,
+      },
+    };
+  }, []);
+
+  const updateAutoGroupCol = () => {
+ 
+    console.log('clicked')
+
+    let newDefs = [{ field: 'age' }, { field: 'date' }, { field: 'country' }];
+
+    gridRef.current.api.setAutoGroupColumnDef(newDefs)
+    // gridRef.current.api.setColumnDefs(newDefs);
+  };
+
+  const onGridReady = useCallback((params) => {
+    fetch("https://www.ag-grid.com/example-assets/olympic-winners.json")
+      .then((resp) => resp.json())
+      .then((data) => setRowData(data));
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <button onClick={updateAutoGroupCol}>Update autoGroupColumnDef</button>
+      <div style={gridStyle} className="ag-theme-alpine">
+        <AgGridReact
+          ref={gridRef}
+          rowData={rowData}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          autoGroupColumnDef={autoGroupColumnDef}
+          groupDisplayType={"multipleColumns"}
+          animateRows={true}
+          onGridReady={onGridReady}
+        ></AgGridReact>
+      </div>
+    </div>
+  );
+};
+
+export default App;
