fd1b873b4aa17f6f85156a00c5381eaa5c9d7937
abdiWali <abdicodes94@gmail.com>
2022-06-22 14:15:36 +0100

    updated ticket t2t-6754

---
+++ projects/t2t-6754/ag-grid.config.sh
@@ -0,0 +1,4 @@
+TICKET="t2t-6754"
+FRAMEWORK="react"
+DOCS_EXAMPLE="range-selection"
+AG_GRID_INSTALLATION_METHOD="packages"
---
+++ projects/t2t-6754/src/index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-enterprise';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import './styles.css';
+
+
+
+const rowSpan = (params) => {
+  var athlete = params.data.athlete;
+  if (athlete === 'Aleksey Nemov') {
+    // have all Russia age columns width 2
+    return 2;
+  } else if (athlete === 'Ryan Lochte') {
+    // have all United States column width 4
+    return 3;
+  } else {
+    // all other rows should be just normal
+    return 1;
+  }
+};
+
+
+
+const GridExample = () => {
+  const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
+  const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
+  const [rowData, setRowData] = useState();
+  const [columnDefs, setColumnDefs] = useState([
+    {
+      field: 'athlete',
+      rowSpan: rowSpan,
+      cellClassRules: {
+        'cell-span': "value==='Aleksey Nemov' || value==='Ryan Lochte'",
+      },
+      width: 200,
+    },
+    { field: 'age' },
+
+  ]);
+  const defaultColDef = useMemo(() => {
+    return {
+      flex: 1,
+      minWidth: 100,
+      editable: true
+    };
+  }, []);
+
+  const onGridReady = useCallback((params) => {
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => setRowData(data.slice(0, 10)));
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div style={gridStyle} className="ag-theme-alpine">
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          enableRangeSelection
+          suppressRowTransform
+          onGridReady={onGridReady}
+        ></AgGridReact>
+      </div>
+    </div>
+  );
+};
+
+render(<GridExample></GridExample>, document.querySelector('#root'));
