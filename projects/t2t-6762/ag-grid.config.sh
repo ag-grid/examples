65c2af175757782e24445039f2dd46933ac1b827
abdiWali <abdicodes94@gmail.com>
2022-06-29 10:04:54 +0100

    updated ticket t2t-6762

---
+++ projects/t2t-6762/ag-grid.config.sh
@@ -0,0 +1,4 @@
+TICKET="t2t-6762"
+FRAMEWORK="react"
+DOCS_EXAMPLE="server-side-row-model"
+AG_GRID_INSTALLATION_METHOD="packages"
--- projects/t2t-6762/src/index.js
+++ projects/t2t-6762/src/index.js
@@ -1,51 +1,13 @@
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+'use strict';
+
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-enterprise';
-import * as agGrid from 'ag-grid-community';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
 
-const gridOptions = {
-  columnDefs: [
-    { field: 'athlete', minWidth: 220 },
-    { field: 'country', minWidth: 200 },
-    { field: 'year' },
-    { field: 'sport', minWidth: 200 },
-    { field: 'gold' },
-    { field: 'silver' },
-    { field: 'bronze' },
-  ],
-
-  defaultColDef: {
-    flex: 1,
-    minWidth: 100,
-  },             
-  animateRows: true,
-  alwaysShowVerticalScroll: true,
-  // suppressAutoSize: false,
-  // autoSizeColumnsToFit: false,
-  cacheBlockSize: 10,
-  maxBlocksInCache: 2,
-  rowModelType: 'serverSide',
-  serverSideStoreType: 'partial',
-};
-
-// setup the grid after the page has finished loading
-var gridDiv = document.querySelector('#myGrid');
-new agGrid.Grid(gridDiv, gridOptions);
-
-fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
-  .then((response) => response.json())
-  .then(function (data) {
-    // setup the fake server with entire dataset
-    var fakeServer = createFakeServer(data);
-
-    // create datasource with a reference to the fake server
-    var datasource = createServerSideDatasource(fakeServer);
-
-    // register the datasource with the grid
-    gridOptions.api.setServerSideDatasource(datasource);
-  });
-
-function createServerSideDatasource(server) {
+const createServerSideDatasource = (server) => {
   return {
     getRows: (params) => {
       console.log(
@@ -54,10 +16,8 @@ function createServerSideDatasource(server) {
           ', endRow = ' +
           params.request.endRow
       );
-
       // get data for request from our fake server
       var response = server.getData(params.request);
-
       // simulating real server call with a 500ms delay
       setTimeout(function () {
         if (response.success) {
@@ -72,17 +32,15 @@ function createServerSideDatasource(server) {
       }, 1000);
     },
   };
-}
+};
 
-function createFakeServer(allData) {
+const createFakeServer = (allData) => {
   return {
     getData: (request) => {
       // in this simplified fake server all rows are contained in an array
       var requestedRows = allData.slice(request.startRow, request.endRow);
-
       // here we are pretending we don't know the last row until we reach it!
       var lastRow = getLastRowIndex(request, requestedRows);
-
       return {
         success: true,
         rows: requestedRows,
@@ -90,12 +48,63 @@ function createFakeServer(allData) {
       };
     },
   };
-}
+};
 
-function getLastRowIndex(request, results) {
+const getLastRowIndex = (request, results) => {
   if (!results) return undefined;
   var currentLastRow = (request.startRow || 0) + results.length;
-
   // if on or after the last block, work out the last row, otherwise return 'undefined'
   return currentLastRow < (request.endRow || 0) ? currentLastRow : undefined;
-}
+};
+
+const GridExample = () => {
+  const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
+  const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
+
+  const [columnDefs, setColumnDefs] = useState([
+    { field: 'athlete', width: 200 },
+    { field: 'country', width: 200 },
+    { field: 'year' },
+    { field: 'sport', width: 200 },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+  ]);
+  const defaultColDef = useMemo(() => {
+    return {
+      // flex: 1,
+      minWidth: 100,
+    };
+  }, []);
+
+  const onGridReady = useCallback((params) => {
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => {
+        // setup the fake server with entire dataset
+        var fakeServer = createFakeServer(data);
+        // create datasource with a reference to the fake server
+        var datasource = createServerSideDatasource(fakeServer);
+        // register the datasource with the grid
+        params.api.setServerSideDatasource(datasource);
+      });
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div style={gridStyle} className='ag-theme-alpine-dark'>
+        <AgGridReact
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          rowModelType={'serverSide'}
+          serverSideStoreType={'partial'}
+          cacheBlockSize={50}
+          maxBlocksInCache={2}
+          onGridReady={onGridReady}
+        ></AgGridReact>
+      </div>
+    </div>
+  );
+};
+
+render(<GridExample></GridExample>, document.querySelector('#root'));
